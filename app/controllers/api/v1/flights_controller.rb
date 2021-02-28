be7075ae7aa5a3a216ef63f8f9d9dbc977671dfa
jrog20 <jaimerogstad@gmail.com>
2021-02-28 12:02:09 -0500

    First commit

---
+++ app/controllers/api/v1/flights_controller.rb
@@ -0,0 +1,34 @@
+class API::V1::FlightsController < ApplicationController
+  def index
+    @flights = Flight.all
+    render json: @flights, status: 200
+  end
+
+  def show
+    @flight = Flight.find(params[:id])
+    render json: @flight, status: 200
+  end
+
+  def create
+    @flight = Flight.create(flight_params)
+    render json: @flight, status: 200
+  end
+
+  def update
+    @flight = Flight.find(params[:id])
+    @flight.update(flight_params)
+    render json: @flight, status: 200
+  end
+
+  def destroy
+    @flight = Flight.find(params[:id])
+    @flight.delete
+    render json: {flightId: @flight.id}
+  end
+
+  private
+  def flight_params
+    params.require(:flight).permit(:body)
+  end
+
+end
